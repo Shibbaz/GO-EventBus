b4c1effd4eddd0eecfc0571135761d589cbee607
Shibbaz <kmosc@protonmail.com>
2024-03-29 04:33:36 +0100

    refactoring

--- main.go
+++ main.go
@@ -5,34 +5,21 @@ import (
 	. "examples"
 	"fmt"
 	. "store"
-	. "stream"
 	"sync"
 	"time"
 )
 
-func Setup(wg sync.WaitGroup, store *Store, index int) {
-	wg.Add(1)
-	node := make(chan Stream, 1)
-	go func(nodeChan chan Stream, ws *sync.WaitGroup, j int) {
-		event := NewEvent(EventArgs{"id": j, "price": 200000}, HouseWasSold{})
-		data := NewStream(event, j)
-		defer ws.Done()
-		data.Append(nodeChan)
-	}(node, &wg, index)
-	store.Send(node)
-	wg.Wait()
-	close(node)
-}
-
 func main() {
 	var wg sync.WaitGroup
 	start := time.Now()
-	const SERVER_NUM = 100000
+	const SERVER_NUM = 1000
 	store := Store{
 		Dispatcher: &EventsDispatcher,
 	}
 	for i := 0; i < SERVER_NUM; i++ {
-		go Setup(wg, &store, i)
+		wg.Add(1)
+		event := NewEvent(EventArgs{"id": i, "price": 200000}, HouseWasSold{})
+		store.Publish(&wg, event, i)
 	}
 
 	elapsed := time.Since(start)
