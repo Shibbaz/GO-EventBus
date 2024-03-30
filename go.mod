ff54b9ebda61356e9e53d64e9c7e00175de81ad5
Shibbaz <kmosc@protonmail.com>
2024-03-30 06:07:31 +0100

    add nuke-memory arena

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.22.1
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/ortuman/nuke v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,53 +4,36 @@ import (
 	. "events"
 	. "examples"
 	"fmt"
-	"log"
-	"net/http"
 	. "store"
 	. "stream"
 	"sync"
 	"time"
 
-	"github.com/gorilla/mux"
+	"github.com/ortuman/nuke"
 )
 
 func main() {
+	arena := nuke.NewMonotonicArena(512*1024, 80)
+
+	defer arena.Reset(true)
 	var wg sync.WaitGroup
 	start := time.Now()
 	const SERVER_NUM = 100000
 	store := Store{
 		Dispatcher: &EventsDispatcher,
 	}
-	var node chan Stream
-	node = make(chan Stream)
+	chanSlice := nuke.MakeSlice[chan Stream](arena, 0, SERVER_NUM)
+
+	nodeRef := nuke.New[chan Stream](arena)
+	*nodeRef = make(chan Stream)
 	for i := 0; i < SERVER_NUM; i++ {
 		wg.Add(1)
 		event := NewEvent(EventArgs{"id": i, "price": 200000}, HouseWasSold{})
-		node = store.Publish(node, &wg, event, i)
+		*nodeRef = store.Publish(*nodeRef, &wg, event, i)
+		chanSlice = nuke.SliceAppend(arena, chanSlice, *nodeRef)
 	}
 	wg.Wait()
 	elapsed := time.Since(start)
 	fmt.Printf("Elapsed time: %s\n", elapsed)
 	time.Sleep(200 * time.Millisecond)
-	handler := EventHandler{
-		Store: Store{
-			Dispatcher: &EventsDispatcher,
-		},
-		Node: make(chan Stream),
-	}
-	mux := mux.NewRouter()
-
-	mux.Handle("/event", &handler)
-
-	srv := &http.Server{
-		Handler: mux,
-		Addr:    "127.0.0.1:8000",
-		// Good practice: enforce timeouts for servers you create!
-		WriteTimeout: 15 * time.Second,
-		ReadTimeout:  15 * time.Second,
-	}
-
-	log.Fatal(srv.ListenAndServe())
-	log.Print("Listening...")
-
 }
