0c3a8a3d306262c02793767bdd852b4259c8c4f3
Shibbaz <kmosc@protonmail.com>
2024-04-19 15:45:53 +0200

    refactoring!

--- subscribe.go
+++ subscribe.go
@@ -1,43 +1,13 @@
 package GOEventBus
 
 import (
-	"bytes"
-	"encoding/gob"
 	"encoding/json"
-	"log"
-
-	"github.com/google/uuid"
-	"github.com/pion/webrtc/v2"
 )
 
 // Usecase of datachannel, event is encoded into bytes and send to another peer (look into Publish func)
 func (eventstore *EventStoreNode) Subscribe(event Event) {
-	ordered := false
-	maxRetransmits := uint16(0)
-	var buffer bytes.Buffer
-	enc := gob.NewEncoder(&buffer)
-	options := &webrtc.DataChannelInit{
-		Ordered:        &ordered,
-		MaxRetransmits: &maxRetransmits,
-	}
-	dc, err := eventstore.connection.CreateDataChannel(uuid.New().String(), options)
-	if err != nil {
-		panic(err)
-	}
-	dc.OnOpen(func() {
-		log.Printf("Channel %s was opened", dc.Label())
-		err := enc.Encode(event)
-		if err != nil {
-			panic(err)
-		}
-		dc.Send(buffer.Bytes())
-	})
-	dc.OnMessage(func(msg webrtc.DataChannelMessage) {
-		eventstore.Listner.OnBye <- true
-		dc.Send([]byte{})
-		dc.Close()
 
-	})
+	eventstore.HandleDataChannel("subscribe", &event)
 	offer, err := eventstore.connection.CreateOffer(nil)
 	if err != nil {
 		panic(err)
